735871dae1c036aa8c43c7040b5b8c0d294c17ee
GeunTae KIM <60951051+Rootstar07@users.noreply.github.com>
2020-12-19 18:22:56 +0900

    2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import potato from './potato'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <potato />
   </React.StrictMode>,
   document.getElementById('root')
 );
